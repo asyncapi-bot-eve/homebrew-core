451109f2c1fc786e93e2c9d98e564783d010ffcd
asyncapi-bot-eve <bot+eve@asyncapi.io>
2022-09-13 23:25:14 +0200

    asyncapi 0.22.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/asyncapi.rb
+++ Formula/asyncapi.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Asyncapi < Formula
   desc "All in one CLI for all AsyncAPI tools"
   homepage "https://github.com/asyncapi/cli"
-  url "https://registry.npmjs.org/@asyncapi/cli/-/cli-0.22.2.tgz"
-  sha256 "9e69b9cacc4ebdb866e4605d3c221d55ab8a791dae432a1880f2919c45948a98"
+  url "https://registry.npmjs.org/@asyncapi/cli/-/cli-0.22.4.tgz"
+  sha256 "b5741e0d4b992e72649b35737d4dad988c6f2d372396ff3f693b58f731c34e19"
   license "Apache-2.0"
 
   bottle do
